97c82f23d2c22f74d37ccd73db18f0f9b0fbeca1
Sebastien Tanguy <tanguy@acreat.com>
2012-11-20 16:15:48 +0100

    Add our new bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Viteloge\AdminBundle\VitelogeAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Viteloge/AdminBundle/VitelogeAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Viteloge\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VitelogeAdminBundle extends Bundle
+{
+}
