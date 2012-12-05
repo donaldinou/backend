d8aba3571a9263d5e371c9d2089a27b49edaba8c
Sebastien Tanguy <tanguy@acreat.com>
2012-12-05 17:26:42 +0100

    Direct creation of privileges from agence

--- src/Viteloge/AdminBundle/Admin/PrivilegeAdmin.php
+++ src/Viteloge/AdminBundle/Admin/PrivilegeAdmin.php
@@ -55,4 +55,16 @@ class PrivilegeAdmin extends Admin
     public function validate(ErrorElement $errorElement, $object)
     {
     }
+
+    public function getNewInstance()
+    {
+        $instance = parent::getNewInstance();
+        $id_agence = $this->request->get('idAgence');
+        if ( ! empty( $id_agence ) ) {
+            $agence_repo = $this->getConfigurationPool()->getContainer()->get('doctrine')->getRepository( 'Viteloge\AdminBundle\Entity\Agence' );
+            $agence = $agence_repo->find( $id_agence );
+            $instance->agence = $agence;
+        }
+        return $instance;
+    }
 }
